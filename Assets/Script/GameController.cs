f7a6aa2eedb81c9d180930b741c7639be41eb9b4
denial <denialwins6@gmail.com>
2017-05-25 22:19:59 +0300

    Homework_14

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject hazard;   
+
+	void Start()
+	{
+		StartCoroutine(SpawnWaves());
+	}
+
+	IEnumerator SpawnWaves()
+	{
+		yield return new WaitForSeconds(1f);
+		while (true)
+		{
+			for (int i = 0; i < 10f; i++)
+			{
+
+				Vector3 camera = Camera.main.transform.position;
+				Vector3 spawnPosition = new Vector3(Random.Range(-camera.x - 5f, camera.x +5f), camera.y +10f, 0f);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(1f);
+			}
+			yield return new WaitForSeconds(10f);
+		}
+	}
+}
---
+++ Assets/Script/MainMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+  
+
+    public void Settings()
+	{
+		
+	}
+
+	public void GamePlay()
+	{
+		SceneManager.LoadScene ("Game Scena");
+	}
+
+	public void About()
+	{
+
+        GetComponent<TextMesh>().text = "Hello World";
+
+    }
+}
